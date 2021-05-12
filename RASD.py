73b405a2dbd4ec39ccf44572e3418bf395190cf7
wmpjrufg <55900120+wmpjrufg@users.noreply.github.com>
2021-05-11 22:58:15 -0300

    Organização gráficos

--- RASD.py
+++ RASD.py
@@ -15,6 +15,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
 import pandas as pd
 
 ################################################################################
@@ -175,11 +176,17 @@ def SAVE_GRAPHIC(NAME, EXT, DPI):
 # PLOTAGEM 1
 def RASD_PLOT_1(DATASET, PLOT_SETUP):
     """
-    THIS FUNCTION PLOT BOXPLOT AND HISTOGRAMS IN A SINGLE GRAPHIC
+    THIS FUNCTION PLOT BOXPLOT AND HISTOGRAMS IN A SINGLE CHART
     
     INPUT: 
     DATASET: RESULTS ABOUT RASD ALGORITHM (NP.ARRAY[? X ?] FLOAT)
     PLOT_SETUP: CONTAINS THE SPECIFICATION OF EACH MODEL OF CHART
+    
+    EXTENSION:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+    Other Parameters >> format
+    AXISES COLOR, LABELS COLOR AND CHART COLOR:
+    https://htmlcolorcodes.com/
 
     OUTPUT:
     N/A
@@ -213,6 +220,7 @@ def RASD_PLOT_1(DATASET, PLOT_SETUP):
     LABELS_SIZE = PLOT_SETUP['LABELS SIZE']     
     LABELS_COLOR = PLOT_SETUP['LABELS COLOR']
     CHART_COLOR = PLOT_SETUP['CHART COLOR']
+    BINS = PLOT_SETUP['BINS']
     KDE = PLOT_SETUP['KDE']
     EXT = PLOT_SETUP['EXTENSION']
     DPI = PLOT_SETUP['DPI']
@@ -224,7 +232,7 @@ def RASD_PLOT_1(DATASET, PLOT_SETUP):
     # PLOT
     FIG, (AX_BOX, AX_HIST) = plt.subplots(2, figsize = (W, H), sharex = True, gridspec_kw = {'height_ratios': (.15, .85)})
     sns.boxplot(DATASET, ax = AX_BOX, color = CHART_COLOR)
-    sns.histplot(DATASET, ax = AX_HIST, kde = KDE, color = CHART_COLOR)
+    sns.histplot(DATASET, ax = AX_HIST, kde = KDE, color = CHART_COLOR, bins = BINS)
     AX_BOX.set(yticks = [])
     AX_BOX.set(xlabel='')
     font = {'fontname': 'Arial',
@@ -243,11 +251,20 @@ def RASD_PLOT_1(DATASET, PLOT_SETUP):
 # PLOTAGEM 2
 def RASD_PLOT_2(DATASET, PLOT_SETUP):
     """
-    THIS FUNCTION PLOT SCATTER PLOT R AND G PARAMETERS ABOUT I INDEX
+    THIS FUNCTION PLOT SCATTER CHART X AND Z VARIABLES A FUNCTION OF I VALUE
     
     INPUT: 
     DATASET: RESULTS ABOUT RASD ALGORITHM (NP.ARRAY[? X ?] FLOAT)
     PLOT_SETUP: CONTAINS THE SPECIFICATION OF EACH MODEL OF CHART
+    
+    LOC LEGEND: 
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.legend.html
+    Other Parameters >> loc
+    EXTENSION:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+    Other Parameters >> format
+    AXISES COLOR AND LABELS COLOR:
+    https://htmlcolorcodes.com/
 
     OUTPUT:
     N/A
@@ -296,17 +313,98 @@ def RASD_PLOT_2(DATASET, PLOT_SETUP):
     # CONVERT UNITS OF SIZE FIGURE
     [W, H] = CONVERT_SI_TO_INCHES(W, H)
     # PLOT
-    FIG, AX = plt.subplots(1, figsize = (W, H))
+    FIG, AX = plt.subplots(figsize = (W, H))
     sns.scatterplot(data = DATASET, x = X_DATA, y = Y_DATA, hue = 'I')
     font = {'fontname': 'Arial',
         'color':  LABELS_COLOR,
-        'weight': 'normal',
+        'weight': 'bold',
         'size': LABELS_SIZE}
     AX.set_xlabel(X_AXIS_LABEL, fontdict = font)
     AX.set_ylabel(Y_AXIS_LABEL, fontdict = font)
     AX.tick_params(axis= 'x', labelsize = X_AXIS_SIZE, colors = AXISES_COLOR)
     AX.tick_params(axis= 'y', labelsize = Y_AXIS_SIZE, colors = AXISES_COLOR)
-    AX.legend(loc = LOC_LEGEND)
-    AX.legend(title = TITLE_LEGEND)
+    AX.legend(loc = LOC_LEGEND, title = TITLE_LEGEND)
+    # SAVEFIG
+    SAVE_GRAPHIC(NAME, EXT, DPI)
+
+# PLOTAGEM 3
+def RASD_PLOT_3(DATASET, PLOT_SETUP):
+    """
+    THIS FUNCTION PLOT SCATTER CHART X AND Z VARIABLES A FUNCTION OF G VALUE
+    
+    INPUT: 
+    DATASET: RESULTS ABOUT RASD ALGORITHM (NP.ARRAY[? X ?] FLOAT)
+    PLOT_SETUP: CONTAINS THE SPECIFICATION OF EACH MODEL OF CHART
+    
+    COLOR MAP: 
+    https://matplotlib.org/stable/tutorials/colors/colormaps.html
+    Other Parameters >> loc
+    EXTENSION:
+    https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+    Other Parameters >> format
+    AXISES COLOR AND LABELS COLOR:
+    https://htmlcolorcodes.com/
+
+    OUTPUT:
+    N/A
+
+    EXAMPLE:
+    # PLOT SETUP
+    PLOT_SETUP = {'NAME': 'WANDER',
+                    'EXTENSION': '.svg',
+                    'DPI': 600,
+                    'WIDTH': 0.20, 
+                    'HEIGHT': 0.10,              
+                    'X DATA': 'S_0',
+                    'Y DATA': 'R_0',
+                    'X AXIS SIZE': 20,
+                    'Y AXIS SIZE': 20,
+                    'AXISES COLOR': '#000000',
+                    'X AXIS LABEL': '$S_0$',
+                    'Y AXIS LABEL': '$R_0$',
+                    'LABELS SIZE': 16,
+                    'LABELS COLOR': '#000000',
+                    'C VALUE': 'G_0',
+                    'TRANSPARENCY': 0.8,
+                    'COLOR MAP': 'viridis'}
+    # RESULTS
+    DATASET = RESULTS_RASD
+    # CALL PLOT
+    RASD.RASD_PLOT_3(DATASET, PLOT_SETUP)
+    """
+    # SETUP CHART
+    NAME = PLOT_SETUP['NAME']
+    EXT = PLOT_SETUP['EXTENSION']
+    DPI = PLOT_SETUP['DPI']
+    W = PLOT_SETUP['WIDTH']
+    H = PLOT_SETUP['HEIGHT']
+    X_DATA = PLOT_SETUP['X DATA']
+    Y_DATA = PLOT_SETUP['Y DATA']
+    X_AXIS_SIZE = PLOT_SETUP['X AXIS SIZE']
+    Y_AXIS_SIZE = PLOT_SETUP['Y AXIS SIZE']
+    AXISES_COLOR = PLOT_SETUP['AXISES COLOR']
+    X_AXIS_LABEL = PLOT_SETUP['X AXIS LABEL']
+    Y_AXIS_LABEL = PLOT_SETUP['Y AXIS LABEL']
+    LABELS_SIZE = PLOT_SETUP['LABELS SIZE']
+    LABELS_COLOR = PLOT_SETUP['LABELS COLOR']
+    C_VALUE = PLOT_SETUP['C VALUE']
+    TRANSPARENCY = PLOT_SETUP['TRANSPARENCY']
+    COLOR_MAP = PLOT_SETUP['COLOR MAP']
+    # CONVERT UNITS OF SIZE FIGURE
+    [W, H] = CONVERT_SI_TO_INCHES(W, H)
+    # PLOT
+    AUX = plt.Normalize(DATASET[C_VALUE].min(), DATASET[C_VALUE].max())
+    FIG, AX = plt.subplots(figsize = (W, H))
+    plt.scatter(x = DATASET[X_DATA], y = DATASET[Y_DATA], c = DATASET[C_VALUE], cmap = COLOR_MAP, alpha = TRANSPARENCY)
+    font = {'fontname': 'Arial',
+        'color':  LABELS_COLOR,
+        'weight': 'bold',
+        'size': LABELS_SIZE}
+    AX.set_xlabel(X_AXIS_LABEL, fontdict = font)
+    AX.set_ylabel(Y_AXIS_LABEL, fontdict = font)
+    AX.tick_params(axis= 'x', labelsize = X_AXIS_SIZE, colors = AXISES_COLOR)
+    AX.tick_params(axis= 'y', labelsize = Y_AXIS_SIZE, colors = AXISES_COLOR)
+    AUX1 =  ScalarMappable(norm = AUX, cmap = COLOR_MAP)
+    FIG.colorbar(AUX1, ax = AX)
     # SAVEFIG
     SAVE_GRAPHIC(NAME, EXT, DPI)
